701bc300a697e7c8c04dcded3384fdfcd00bb963
ZonisFacundo <facundozonis7@gmail.com>
2025-04-09 19:52:16 +0000

    casi terminada conexion http IO con Kernel, error al recibir rspuesta

--- io/io.go
+++ io/io.go
@@ -7,4 +7,11 @@ import (
 func main() {
 
 	utilsIO.ConfigurarLogger()
+
+	/*
+	   conexion entre IO (Client) con Kernel (Server)
+	   enviamos handshake con datos del modulo y esperamos respuesta
+	*/
+	utilsIO.HandshakeAKernel("pepe", "127.0.0.1", 8003)
+
 }
