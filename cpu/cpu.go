0581516e606800bb82724bb4567c972af8f13d75
Bren <bybanez@frba.utn.edu.ar>
2025-04-13 01:02:17 +0000

    establezco conexion CPUAMEM y CPUAKERNEL

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -6,4 +6,12 @@ import (
 
 func main() {
 	utilsCPU.ConfigurarLogger()
+
+	/*
+	   conexion entre CPU (Client) con Kernel (Server)
+	   enviamos handshake con datos del modulo y esperamos respuesta
+	*/
+
+	utilsCPU.HandshakeCPUAKernel("127.0.0.1", 8001) 
+	utilsCPU.HandshakeCPUAMemoria("NOOP","127.0.0.1", 8002)
 }
