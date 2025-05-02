de693c5fd26bfe3a64b2a49e8c49d3248e966d20
Manuel Acero <macero@frba.utn.edu.ar>
2025-05-02 19:13:23 +0000

    avances en IO

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/sisoputnfrba/tp-golang/kernel/globals"
 	"github.com/sisoputnfrba/tp-golang/utils/utilsKernel"
@@ -19,26 +20,22 @@ import (
 func main() {
 	globals.CargarConfig("./kernel/globals/config.json")
 	utilsKernel.ConfigurarLogger()
-	//utilsKernel.CrearPCB(2, "github.com/sisoputnfrba/tp-golang/archi.txt") ///home/utnso/tp-2025-1c-NutriGO/archi.txt
 	archivo := os.Args[1]
 	tamanio, err := strconv.Atoi(os.Args[2])
 	if err != nil {
-		log.Fatalf("❌ Tamaño inválido: %v", err)
+		log.Fatalf("Tamaño inválido: %v", err)
 	}
 	println(archivo)
 	println(tamanio)
 	utilsKernel.CrearPCB(tamanio, archivo)
 
 	go utilsKernel.IniciarPlanifcador()
-	/*
-		time.Sleep(1 * time.Second)
-		utilsKernel.PeticionClienteKERNELServidorIO("127.0.0.1", 8003)
-
-		time.Sleep(2 * time.Second)
-	*/
-
-	//http.HandleFunc("/handshake", utilsKernel.RetornoClienteIOServidorKERNEL)
-	//http.HandleFunc("POST /IO", utilsKernel.RetornoClienteIOServidorKERNEL)
+	go func() {
+		time.Sleep(4 * time.Second)
+		utilsKernel.PeticionClienteKERNELServidorIO("127.0.0.1", 8003, 8)
+	}()
+	http.HandleFunc("/handshake", utilsKernel.RetornoClienteIOServidorKERNEL)
+	http.HandleFunc("POST /IO", utilsKernel.RetornoClienteIOServidorKERNEL)
 	http.HandleFunc("POST /handshake", utilsKernel.RetornoClienteCPUServidorKERNEL)
 	http.HandleFunc("POST /PCB", utilsKernel.RetornoClienteCPUServidorKERNEL2)
 	log.Printf("Servidor corriendo.\n")
