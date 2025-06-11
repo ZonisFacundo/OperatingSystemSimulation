513be004905c8bf01c3e7389550634adc7a58d74
mspena <mapena@frba.utn.edu.ar>
2025-06-11 20:46:32 +0000

    modificacion del main

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
+	"time"
 
 	"github.com/sisoputnfrba/tp-golang/cpu/globals"
 	"github.com/sisoputnfrba/tp-golang/cpu/instruction_cycle"
@@ -13,10 +13,8 @@ import (
 )
 
 // falta hacer la conexion del lado del cpu como servidor hacia el kernel pero no sabia donde hacerlas ni les queria romper el codigo =)
-func main() {
 
-	var wg sync.WaitGroup
-	procesoListo := make(chan bool)
+func main() {
 
 	if len(os.Args) < 2 {
 		log.Fatal("Error: Debe indicar el identificador de la CPU como argumento, por ejemplo: ./cpu cpuX")
@@ -30,31 +28,24 @@ func main() {
 
 	utilsCPU.EnvioPortKernel(globals.ClientConfig.Ip_kernel, globals.ClientConfig.Port_kernel, globals.ClientConfig.Instance_id, globals.ClientConfig.Port_cpu, globals.ClientConfig.Ip_cpu)
 
-	http.HandleFunc("/KERNELCPU", func(w http.ResponseWriter, r *http.Request) {
-		utilsCPU.RecibirPCyPID(w, r)
-		procesoListo <- true
-	})
-
 	go func() {
+		http.HandleFunc("/KERNELCPU", utilsCPU.RecibirPCyPID)
 		log.Printf("Servidor corriendo, esperando PID y PC de Kernel.")
-		err := http.ListenAndServe(fmt.Sprintf(":%d", globals.ClientConfig.Port_cpu), nil)
-		if err != nil {
-			log.Fatalf("Error al iniciar servidor HTTP: %v", err)
-		}
+		http.ListenAndServe(fmt.Sprintf(":%d", globals.ClientConfig.Port_cpu), nil)
 	}()
 
-	// Goroutine que espera el proceso y ejecuta el ciclo de instrucción
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			<-procesoListo // espera a que llegue un procesos (habria que hacer algo para que no espere eternamente)
-			log.Printf("Iniciando ciclo de instrucción para PID: %d, PC: %d", globals.Instruction.Pid, globals.Instruction.Pc)
+	primeraEjecucion := true
+
+	// Ciclo principal
+	for {
+		if primeraEjecucion {
 			instruction_cycle.Fetch(globals.Instruction.Pid, globals.Instruction.Pc, globals.ClientConfig.Ip_memory, globals.ClientConfig.Port_memory)
-			instruction_cycle.Decode(globals.ID)
-			instruction_cycle.Execute(globals.ID)
+			primeraEjecucion = false
 		}
-	}()
 
-	wg.Wait()
+		instruction_cycle.Decode(globals.ID)
+		instruction_cycle.Execute(globals.ID)
+
+		time.Sleep(100 * time.Millisecond)
+	}
 }
